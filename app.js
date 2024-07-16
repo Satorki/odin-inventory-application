92d29454d88c43c5ec0e8c1cfd3cdc4b8a588dd3
Satorki <sebastian.kedzier@gmail.com>
2024-07-16 14:31:39 +0200

    index view added

--- app.js
+++ app.js
@@ -15,12 +15,13 @@ async function main() {
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+const partsRouter = require("./routes/parts");
 
 var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set("view engine", "pug");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/parts", partsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ controllers/partController.js
@@ -0,0 +1,47 @@
+const Part = require("../models/part");
+const asyncHandler = require("express-async-handler");
+
+exports.index = asyncHandler(async (req, res, next) => {
+  const [numParts, numCategories] = await Promise.all([
+    Part.countDocuments({}).exec(),
+    Category.countDocuments({}).exec(),
+  ]);
+
+  res.render("index", {
+    title: "Computer Parts Shop",
+    part_count: numParts,
+    category_count: numCategories,
+  });
+});
+
+exports.part_list = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Part list");
+});
+
+exports.part_detail = asyncHandler(async (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: Part detail: ${req.params.id}`);
+});
+
+exports.part_create_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Part create GET");
+});
+
+exports.part_create_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Part create POST");
+});
+
+exports.part_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Part delete GET");
+});
+
+exports.part_delete_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Part delete POST");
+});
+
+exports.part_update_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Part update GET");
+});
+
+exports.part_update_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Part update POST");
+});
