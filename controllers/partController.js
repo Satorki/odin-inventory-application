1b3bd9caad07599a041bc1bb67f37bd8230c06ad
Satorki <sebastian.kedzier@gmail.com>
2024-07-17 12:30:50 +0200

    The Create Part form added

--- controllers/partController.js
+++ controllers/partController.js
@@ -1,10 +1,12 @@
 const Part = require("../models/part");
+const Category = require("../models/category");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.index = asyncHandler(async (req, res, next) => {
   const [numParts, numCategories] = await Promise.all([
     Part.countDocuments({}).exec(),
-    // Category.countDocuments({}).exec(),
+    Category.countDocuments({}).exec(),
   ]);
 
   res.render("index", {
@@ -29,12 +31,72 @@ exports.part_detail = asyncHandler(async (req, res, next) => {
 });
 
 exports.part_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Part create GET");
+  const [allCategories] = await Promise.all([
+    Category.find().sort({ name: 1 }).exec(),
+  ]);
+
+  res.render("part_form", {
+    title: "Create Part",
+    categories: allCategories,
+  });
 });
 
-exports.part_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Part create POST");
-});
+exports.part_create_post = [
+  (req, res, next) => {
+    if (!Array.isArray(req.body.genre)) {
+      req.body.genre =
+        typeof req.body.genre === "undefined" ? [] : [req.body.genre];
+    }
+    next();
+  },
+
+  body("name", "Name must not be empty.").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("category", "Category must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("price", "Price must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("stock", "Stock must not be empty but can be 0")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const part = new Part({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      stock: req.body.stock,
+    });
+
+    if (!errors.isEmpty()) {
+      const [allCategories] = await Promise.all([
+        Category.find().sort({ name: 1 }).exec(),
+      ]);
+
+      for (const category of allCategories) {
+        if (book.category.indexOf(category._id) > -1) {
+          category.checked = "true";
+        }
+      }
+      res.render("part_form", {
+        title: "Create Part",
+        categories: allCategories,
+        errors: errors.array(),
+      });
+    } else {
+      await part.save();
+      res.redirect(part.url);
+    }
+  }),
+];
 
 exports.part_delete_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Part delete GET");
